aaa4770320a07075cab0eceebaed2dbb43bb1686
Ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-06-03 20:56:30 -0200

    add Not method

--- Pandemonium/Pandemonium.Test/Extensions/Boolean/AndTest.cs
+++ Pandemonium/Pandemonium.Test/Extensions/Boolean/AndTest.cs
@@ -7,5 +7,14 @@ namespace Pandemonium.Test.Extensions.Boolean
     {
         [Fact]
         public void Should_Be_True_Given_2_True_Values() => Assert.True(true.And(true));
+
+        [Theory]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public void Should_Be_False_When_Both_Values_Are_Not_True_At_Same_Time(bool firstValue, bool secondValue)
+        {
+            Assert.False(firstValue.And(secondValue));
+        }
     }
 }
---
+++ Pandemonium/Pandemonium.Test/Extensions/Boolean/FalseTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Boolean
+{
+    public class FalseTest
+    {
+        [Fact]
+        public void Should_Be_True_Given_False_Valus() => Assert.True(false.False());
+
+        [Fact]
+        public void Should_Be_False_Given_True_Valus() => Assert.False(true.False());
+    }
+}
