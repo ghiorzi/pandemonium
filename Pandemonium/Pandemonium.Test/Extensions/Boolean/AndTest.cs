113fd63f1759d09da92fa591e8a138f2e33fa35e
Ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-06-03 20:46:48 -0200

    add And method

---
+++ Pandemonium/Pandemonium.Test/Extensions/Boolean/AndTest.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Boolean
+{
+    public class AndTest
+    {
+        [Fact]
+        public void Should_Be_True_Given_2_True_Values() => Assert.True(true.And(true));
+    }
+}
