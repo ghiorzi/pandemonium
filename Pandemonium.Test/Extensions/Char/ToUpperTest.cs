47aad6b72cc8e18c6e3ca4e3b44ac72c616fe503
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-06-30 21:38:33 -0300

    add to upper method

---
+++ Pandemonium.Test/Extensions/Char/ToUpperTest.cs
@@ -0,0 +1,11 @@
+using Xunit;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Char
+{
+    public class ToUpperTest
+    {
+        [Fact]
+        public void Should_Be_True_Given_A_Lowercase_Letter() => Assert.True('a'.ToUpper() == 'A');
+    }
+}
