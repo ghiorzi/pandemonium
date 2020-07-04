2187fde93785e647e2c274f17b4d594efef9fb8d
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-07-04 18:27:13 -0300

    feat: add any method for dictionary

---
+++ Pandemonium/Extensions/IDictionary/IDictionary.Any.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Pandemonium.Extensions
+{
+    public static partial class Methods
+    {
+        /// <summary>
+        /// Returns true if value has at least one of the keys, otherwise returns false
+        /// </summary>
+        public static bool Any<TKey, TValue>(this IDictionary<TKey, TValue> @this, params TKey[] keys)
+        {
+            foreach (TKey value in keys)
+            {
+                if (@this.ContainsKey(value))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
