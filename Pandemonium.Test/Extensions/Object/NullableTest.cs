ceb5f080c4efe923a95c588de6d3fa01874165c5
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-07-28 21:54:55 -0300

    feat: add nullable method for object

---
+++ Pandemonium.Test/Extensions/Object/NullableTest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using System;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Object
+{
+    public class NullableTest
+    {
+        [Fact]
+        public void Should_Be_True_Given_A_Nullable_Value() 
+        {
+            object value = "anything";
+
+            Assert.True(value.Nullable());
+        }
+
+        [Fact]
+        public void Should_Be_False_Given_A_Non_Nullable_Value() => Assert.False(DateTime.Now.Nullable());
+    }
+}
