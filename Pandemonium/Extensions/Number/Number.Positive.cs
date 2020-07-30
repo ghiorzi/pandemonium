47f7d62768bc06adb663656786ed0637d25904db
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-07-30 20:43:16 -0300

    feat: add positive method for numbers

---
+++ Pandemonium/Extensions/Number/Number.Positive.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+namespace Pandemonium.Extensions
+{
+    public static partial class Methods
+    {
+        /// <summary>
+        /// Returns true if value is a positive number
+        /// </summary>
+        public static bool Positive(this sbyte @this) 
+            => @this > 0;
+
+        /// <summary>
+        /// Returns true if value is a positive number
+        /// </summary>
+        public static bool Positive(this short @this) 
+            => @this > 0;
+
+        /// <summary>
+        /// Returns true if value is a positive number
+        /// </summary>
+        public static bool Positive(this int @this) 
+            => @this > 0;
+
+        /// <summary>
+        /// Returns true if value is a positive number
+        /// </summary>
+        public static bool Positive(this long @this) 
+            => @this > 0;
+
+        /// <summary>
+        /// Returns true if value is a positive number
+        /// </summary>
+        public static bool Positive(this float @this) 
+            => @this > 0;
+
+        /// <summary>
+        /// Returns true if value is a positive number
+        /// </summary>
+        public static bool Positive(this double @this) 
+            => @this > 0;
+
+        /// <summary>
+        /// Returns true if value is a positive number
+        /// </summary>
+        public static bool Positive(this decimal @this) 
+            => @this > 0;
+    }
+}
