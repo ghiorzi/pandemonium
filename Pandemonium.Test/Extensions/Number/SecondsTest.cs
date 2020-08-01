3b2bb88f0be8fa7c7c65d8fddc31ea67379a8ce4
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-07-31 22:13:09 -0300

    feat: add seconds method for number

---
+++ Pandemonium.Test/Extensions/Number/SecondsTest.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using System;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Number
+{
+    public class SecondsTest
+    {
+        [Fact]
+        public void Should_Be_True_Given_1_Second() 
+        {
+            Assert.True(((sbyte) 1).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.True(((short) 1).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.True(((int) 1).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.True(((long) 1).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.True(((float) 1.0).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.True(((double) 1.0).Seconds() == TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public void Should_Be_False_Given_2_Seconds() 
+        {
+            Assert.False(((sbyte) 2).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.False(((short) 2).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.False(((int) 2).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.False(((long) 2).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.False(((float) 2.0).Seconds() == TimeSpan.FromSeconds(1));
+            Assert.False(((double) 2.0).Seconds() == TimeSpan.FromSeconds(1));
+        }
+    }
+}
