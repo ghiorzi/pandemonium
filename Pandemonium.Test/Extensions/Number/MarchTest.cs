976e1cd5ee3fcb7f3fbf8cba10f30ba36c5fee5a
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-08-02 16:10:57 -0300

    feat: add march method for int

--- Pandemonium.Test/Extensions/Number/MarchTest.cs
+++ Pandemonium.Test/Extensions/Number/MarchTest.cs
@@ -4,20 +4,20 @@ using Pandemonium.Extensions;
 
 namespace Pandemonium.Test.Extensions.Number
 {
-    public class FebruaryTest
+    public class MarchTest
     {
         [Fact]
-        public void Should_Be_True_Given_February_As_Month() 
+        public void Should_Be_True_Given_March_As_Month() 
         {
-            DateTime date = 28.February(2020);
+            DateTime date = 28.March(2020);
 
-            Assert.True(date == new DateTime(2020, 2, 28));
+            Assert.True(date == new DateTime(2020, 3, 28));
         }
 
         [Fact]
         public void Should_Be_False_Given_July_As_Month() 
         {
-            DateTime date = 28.February(2020);
+            DateTime date = 28.March(2020);
 
             Assert.False(date == new DateTime(2020, 7, 28));
         }
