e87fdd7c023c61ce7f150766ecfa0fef88667a29
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-08-02 16:13:20 -0300

    feat: add april method for int

---
+++ Pandemonium.Test/Extensions/Number/AprilTest.cs
@@ -0,0 +1,25 @@
+using Xunit;
+using System;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Number
+{
+    public class AprilTest
+    {
+        [Fact]
+        public void Should_Be_True_Given_April_As_Month() 
+        {
+            DateTime date = 28.April(2020);
+
+            Assert.True(date == new DateTime(2020, 4, 28));
+        }
+
+        [Fact]
+        public void Should_Be_False_Given_July_As_Month() 
+        {
+            DateTime date = 28.April(2020);
+
+            Assert.False(date == new DateTime(2020, 7, 28));
+        }
+    }
+}
