ecadc0f2be4e9d0d2038a609a0ccae77e1fd9341
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-08-08 21:24:18 -0300

    feat: add not in

--- Pandemonium.Test/Extensions/Date/NotInTest.cs
+++ Pandemonium.Test/Extensions/Date/NotInTest.cs
@@ -5,8 +5,22 @@ using System.Collections.Generic;
 
 namespace Pandemonium.Test.Extensions.Date
 {
-    public class InTest
+    public class NotInTest
     {
+        [Fact]
+        public void Should_Be_Not_In()
+        {
+            var dates = new List<DateTime>();
+
+            // yeah, i'm lazy
+            for (int i = 3; i <= 7; i++)
+                dates.Add(new DateTime(2020, 8, i));
+
+            bool result = new DateTime(2020, 8, 15).NotIn(dates.ToArray());
+
+            Assert.True(result);
+        }
+
         [Fact]
         public void Should_Be_In()
         {
@@ -16,21 +30,7 @@ namespace Pandemonium.Test.Extensions.Date
             for (int i = 3; i <= 7; i++)
                 dates.Add(new DateTime(2020, 8, i));
 
-            bool result = new DateTime(2020, 8, 5).In(dates.ToArray());
-
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void Should_Not_Be_In()
-        {
-            var dates = new List<DateTime>();
-
-            // yeah, i'm lazy
-            for (int i = 3; i <= 7; i++)
-                dates.Add(new DateTime(2020, 8, i));
-
-            bool result = new DateTime(2020, 8, 2).In(dates.ToArray());
+            bool result = new DateTime(2020, 8, 5).NotIn(dates.ToArray());
 
             Assert.False(result);
         }
