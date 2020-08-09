5f0a6f6950dedfe8ee2fc988cc84e7259c8ad126
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-08-08 22:07:11 -0300

    feat: add where

---
+++ Pandemonium.Test/Extensions/String/WhereTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Strings
+{
+    public class WhereTest
+    {
+        [Fact]
+        public void Should_Get_Only_Numbers()
+        {
+            string text = "1 2 3 - sample";
+
+            string number = text.Where(value => value.Number());
+
+            Assert.Equal("123", number);
+        }
+    }
+}
