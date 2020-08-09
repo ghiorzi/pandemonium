7bc9cc297a167220e272d49b0ca8c1d3f88fd8b4
ghiorzi <cagueiandeiefizmoonwalk@gmail.com>
2020-08-08 22:58:02 -0300

    feat: add contains

---
+++ Pandemonium.Test/Extensions/String/ContainsTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using Pandemonium.Extensions;
+
+namespace Pandemonium.Test.Extensions.Strings
+{
+    public class ContainsTest
+    {
+        [Fact]
+        public void Should_Contain()
+        {
+            bool result = "1234".Contains("s", "a", "m", "1");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Should_Not_Contain()
+        {
+            bool result = "1234".Contains("s", "a", "m", "z");
+
+            Assert.False(result);
+        }
+    }
+}
---
+++ Pandemonium/Extensions/String/String.Contains.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Pandemonium.Extensions
+{
+    public static partial class Methods
+    {
+        public static bool Contains(this string @this, string value) 
+            => @this.IndexOf(value) != -1;
+
+        public static bool Contains(this string @this, StringComparison comparison, string value)
+            => @this.IndexOf(value, comparison) != -1;
+
+        public static bool Contains(this string @this, params string[] values)
+        {
+            foreach (string value in values)
+            {
+                bool contains = @this.IndexOf(value) != -1;
+
+                if (contains)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool Contains(this string @this, StringComparison comparison, params string[] values)
+        {
+            foreach (string value in values)
+            {
+                bool contains = @this.IndexOf(value, comparison) != -1;
+
+                if (contains)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
